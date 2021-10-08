d33ab011917d4f47d29016d998492af6b3e8ef28
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-10-08 17:19:21 +0700

    (video 3) menambahkan css, js dan gambar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
   <title>MRP Site | About</title>
 </head>
 <body>
   <h1>Halaman About</h1>
+  <h3>Mochamad Rizky Pratama</h3>
+  <p>mrizkyprtm28@gmail.com</p>
+  <img src="img/profile.jpg" alt="M. Rizky" width="200">
 </body>
 </html>
