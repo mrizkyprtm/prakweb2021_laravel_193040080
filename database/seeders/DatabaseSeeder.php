339fdc4b7f49709a08367d571d31b8c273cf4ddc
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-10-22 13:41:37 +0700

    (video 9) membuat database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,57 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'M. Rizky',
+            'email' => 'mrizkyprtm28@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
     }
 }
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('container')  
-  <h1>Halaman About</h1>
+  <h1 class="mb-5">Halaman About</h1>
   <h3>{{ $name }}</h3>
   <p>{{ $email }}</p>
   <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
