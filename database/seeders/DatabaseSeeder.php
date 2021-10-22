96c6bcaae0319edfe21aa69ac43fcbbc8c464f85
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-10-22 15:09:21 +0700

    (video 10) latihan mengenai Factory dan Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'M. Rizky',
-            'email' => 'mrizkyprtm28@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
 
-        User::create([
-            'name' => 'Pratama',
-            'email' => 'mrpratama@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'M. Rizky',
+        //     'email' => 'mrizkyprtm28@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        // User::create([
+        //     'name' => 'Pratama',
+        //     'email' => 'mrpratama@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -40,40 +42,42 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolor laudantium nesciunt architecto maxime error earum ipsa magnam. Dicta odio minus quibusdam ad eius rerum quasi nesciunt optio quos mollitia magnam illo, harum vero, laborum quia quo autem, voluptatem atque. Cupiditate nesciunt illo dolores quaerat quisquam eos quia? Aliquid saepe mollitia consequuntur sapiente? Omnis non eveniet, vel maxime dolore ad quos hic voluptatem veniam error rem? Deserunt earum quaerat doloribus atque veritatis dolore? Officia commodi veniam quos minus amet culpa pariatur nihil ipsa enim totam excepturi dolor, odit mollitia modi eius eum deserunt adipisci dolores nam necessitatibus. Nulla, voluptatem ducimus!',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
