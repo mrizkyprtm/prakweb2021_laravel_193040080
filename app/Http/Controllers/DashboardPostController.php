452cd0972389b354bbf776a4a5ad350dd14da1d5
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-11-19 20:24:54 +0700

    (video 20) menjalankan fitur update

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -64,6 +64,10 @@ class DashboardPostController extends Controller
      */
     public function show(Post $post)
     {
+        if($post->author->id !== auth()->user()->id) {
+            abort(403);
+        }
+
         return view('dashboard.posts.show', [
             'post' => $post
         ]);
@@ -77,6 +81,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
+        if($post->author->id !== auth()->user()->id) {
+            abort(403);
+        }
+
         return view('dashboard.posts.edit', [
             'post' => $post,
             'categories' => Category::all()
@@ -92,7 +100,25 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = ([
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validatedData = $request->validate($rules);
+        
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::where('id', $post->id)
+            ->update($validatedData);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been updated!');
     }
 
     /**
