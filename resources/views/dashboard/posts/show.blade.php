a2092eb15d64acecfd7d89646ced6782ce5d8a84
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-11-19 19:46:28 +0700

    (video 20) membuat fitur delete

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -8,7 +8,11 @@
 
       <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back To All My Posts</a>
       <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-      <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+      <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+        @method('delete')
+        @csrf
+        <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span> Delete</button>
+      </form>
 
       <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
       <article class="my-3 fs-5">
