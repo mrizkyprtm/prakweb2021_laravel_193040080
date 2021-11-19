e61f7c8e996e974f62fedfa5e57b4648a1aa18b3
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-11-19 20:00:32 +0700

    (video 20) membuat halaman update

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,7 +7,7 @@
       <h1 class="mb-3">{{ $post->title }}</h1>
 
       <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back To All My Posts</a>
-      <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+      <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
       <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
         @method('delete')
         @csrf
